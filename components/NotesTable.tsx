d533ca11173aa27f17d4c7b047c7c343d0451859
Nick Clark <nick.clark@dovetailstudios.com>
2024-02-28 15:01:46 +1300

    Reload server side for dashboard and refactor notes fetch

--- components/NotesTable.tsx
+++ components/NotesTable.tsx
@@ -1,46 +1,11 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { createClient } from "@/utils/supabase/client";
 import { Tables } from "@/types/supabase";
 import Link from "next/link";
 
-const supabase = createClient();
-
-export default function NotesTable() {
-  const [notes, setNotes] = useState<Tables<"notes">[] | null>([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    async function getNotes() {
-      setLoading(true);
-
-      const { data } = await supabase.from("notes").select().order('created_at', { ascending: false }); //.eq('user_id', user.id); can leave off it policy is setup correctly
-
-      if (data && data?.length) {
-        setNotes(data);
-      }
-
-      setLoading(false);
-    }
-    getNotes();
-  }, []);
-
+export default function NotesTable({ notes }: { notes?: Tables<"notes">[] | null }) {
   return (
     <>
-      <div className="h-dvh flex flex-col items-center justify-center -mt-36">
-        <div className="">
-          <Link
-            href="/record"
-            className="flex items-center px-6 py-4 border border-gray-300 hover:border-orange-500 rounded-full"
-          >
-            <span className="block rounded-full bg-orange-500 mr-2">
-              <span className="block w-5 h-5 m-3 rounded-full bg-white"></span>
-            </span>
-            <span className="text-2xl">Record a note</span>
-          </Link>
-        </div>
-      </div>
       {!!notes?.length ? (
         <div className="grid grid-cols-3 gap-10 p-10 bg-gray-200 rounded-3xl mb-8 -mt-32">
           {notes.map((note, i) => (
