0da4dd4b505192bae0fbf86dccee1f29e1ea3f8a
Ricky Hopkins <ricky.hopkins5@gmail.com>
2024-03-11 14:24:03 +1300

    Switch over to supabase next auth helpers

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,16 @@
-import { type NextRequest } from 'next/server';
-import { updateSession } from '@/utils/supabase/middleware';
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { type NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  // Create a Supabase client configured to use cookies
+  const supabase = createMiddlewareClient({ req, res });
+
+  // Refresh session if expired - required for Server Components
+  await supabase.auth.getUser();
+
+  return res;
 }
 
 export const config = {
