c8a31ccc5669e7bbe9270cb685bc92d76d1b70e4
Andrew Hird <andrew.hird@palomagroup.com>
2024-03-12 16:59:57 +1300

    increase size of modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -117,7 +117,7 @@ export default function Modal({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-md sm:p-6 md:max-w-xl">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6 md:max-w-2xl">
                 <div>
                   <div className="mt-3 text-center sm:mt-5">
                     {title && (
