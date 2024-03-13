7c3626cd2af8c59e1bf709ad0a8def750c96691c
Andrew Hird <andrew.hird@palomagroup.com>
2024-03-14 09:44:20 +1300

    Revert "check for user before loading layout"
    This reverts commit ed426bd70623b95e698b45efa482a8db09be6731.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,22 +1,10 @@
 import Header from '@/components/Header';
-import { createClient } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
 
 interface Props {
   children: React.ReactNode;
 }
 
-export default async function AppLayout({ children }: Props) {
-  const supabase = createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user) {
-    return redirect('/login');
-  }
-
+export default function AppLayout({ children }: Props) {
   return (
     <div className={'container min-h-svh layout-container'}>
       <Header />
