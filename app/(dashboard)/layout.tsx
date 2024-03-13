ed426bd70623b95e698b45efa482a8db09be6731
Andrew Hird <andrew.hird@palomagroup.com>
2024-03-13 16:35:37 +1300

    check for user before loading layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,22 @@
 import Header from '@/components/Header';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
 interface Props {
   children: React.ReactNode;
 }
 
-export default function AppLayout({ children }: Props) {
+export default async function AppLayout({ children }: Props) {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect('/login');
+  }
+
   return (
     <div className={'container min-h-svh layout-container'}>
       <Header />
