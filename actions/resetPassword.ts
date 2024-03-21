a3dd153c05318eeae9151402fdb6c26f0a25c34f
Andrew Hird <andrew.hird@palomagroup.com>
2024-03-21 14:08:32 +1300

    use BASE_URL for reset pw link

--- actions/resetPassword.ts
+++ actions/resetPassword.ts
@@ -7,12 +7,8 @@ export const resetPassword = async (formData: FormData) => {
   const email = formData.get('email') as string;
   const supabase = createServerActionClient();
 
-  const defaultUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : 'http://localhost:3000';
-
   const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: `${defaultUrl}/update-password`,
+    redirectTo: `${process.env.BASE_URL}/update-password`,
   });
 
   if (error) {
