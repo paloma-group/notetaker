d18dda31a784b08f195fca43f528eb961d1a5a8b
Hitarth Sharma <hitarth.sharma@outlook.com>
2024-06-11 17:30:29 +1200

    implement on-device transcription that cant accumulate full transcript at the end

--- components/Recorder.tsx
+++ components/Recorder.tsx
@@ -9,8 +9,12 @@ import { formatDate } from '@/utils/date/formatDate';
 import { createNote } from '@/utils/notes/create-note';
 import Image from 'next/image';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { use, useEffect, useRef, useState } from 'react';
 import spinner from '../assets/spinner.svg';
+
+const WHISPER_SAMPLING_RATE = 16_000;
+const MAX_AUDIO_LENGTH = 30; // seconds
+const MAX_SAMPLES = WHISPER_SAMPLING_RATE * MAX_AUDIO_LENGTH;
 
 const Recorder = ({ userId }: { userId: string }) => {
   const { push, replace } = useRouter();
@@ -23,15 +27,19 @@ const Recorder = ({ userId }: { userId: string }) => {
 
   const [title, setTitle] = useState('Record your voice note');
   const [error, setError] = useState<string | null>(null);
-  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
-    null
-  );
   const [isRunning, setIsRunning] = useState(false);
   const [isProcessing, setIsProcessing] = useState(false);
   const seconds = useCounter();
+  const workerRef = useRef<Worker | null>(null);
+  const recorderRef = useRef<MediaRecorder | null>(null);
+  const audioContextRef = useRef<AudioContext | null>(null);
+  const [transcription, setTranscription] = useState('');
+  const [chunks, setChunks] = useState<Blob[]>([]);
+  const [shouldStartRecording, setShouldStartRecording] = useState(false);
 
   const record = searchParams.get('record');
 
+  // Setup serach params
   useEffect(() => {
     const params = new URLSearchParams(searchParams);
     if (record) {
@@ -41,62 +49,204 @@ const Recorder = ({ userId }: { userId: string }) => {
     }
   }, [record]);
 
-  const startRecording = async () => {
-    setIsRunning(true);
-    setError(null);
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-    });
-    const recorder = new MediaRecorder(stream);
-    let audioChunks: any = [];
+  // Setup worker.js for speech recognition
+  useEffect(() => {
+    workerRef.current = new Worker(
+      new URL('../utils/whisper/worker.js', import.meta.url)
+    );
 
-    recorder.ondataavailable = (e) => {
-      audioChunks.push(e.data);
-    };
+    const handleWorkerMessage = (e: MessageEvent) => {
+      const { status, output } = e.data;
 
-    recorder.onstop = async () => {
-      const blob = new Blob(audioChunks, { type: 'audio/mp3' });
-      try {
-        const note = await createNote({
-          userId,
-          audioBlob: blob,
-        });
-
-        if (note) {
-          return push(`/notes/${note.id}`);
-        }
-      } catch (e) {
-        if (e instanceof Error) {
-          setError(e.message);
-        } else {
-          setError(
-            'There was an error with the transcription process, please try again.'
-          );
-        }
-        setIsRunning(false);
-        setIsProcessing(false);
-        seconds.reset();
+      switch (status) {
+        case 'ready':
+          // The worker is ready, you can now start recording or enable UI elements
+          console.log('Worker is ready to process audio data.');
+          break;
+        case 'update':
+          // Log the partial transcript as it's being processed
+          // console.log('Transcription update:', output);
+          break;
+        case 'complete':
+          // Log the final transcript and update the state
+          console.log('Transcription complete:', output);
+          setTranscription((prev) => {
+            console.log(`Previous: ${prev}\n\nTranscription: ${transcription}`)
+            const newTranscript = `${transcription} ${output}` //`${prev?.trim()} ${output}`
+            return newTranscript;
+          }); // Assuming 'output' is the final transcript text
+          if (recorderRef.current?.state === 'recording') {
+            recorderRef.current?.requestData();
+          }
+          break;
+        default:
+          // Handle other statuses or log an unrecognized status
+          console.log(`Received unrecognized status from worker: ${status}`);
       }
     };
-    setMediaRecorder(recorder);
-    recorder.start(1000);
+
+    workerRef.current.addEventListener('message', handleWorkerMessage);
+
+    // Post a message to the worker to load the model
+    workerRef.current.postMessage({ type: 'load' });
+
+    return () => {
+      workerRef.current?.removeEventListener('message', handleWorkerMessage);
+      workerRef.current?.terminate();
+    };
+  }, []);
+
+  // Setup MediaRecorder when recording should be started
+  useEffect(() => {
+    if (recorderRef.current) return;
+
+    if (shouldStartRecording) {
+      if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+          .getUserMedia({ audio: true })
+          .then((stream) => {
+            recorderRef.current = new MediaRecorder(stream);
+            audioContextRef.current = new AudioContext({ sampleRate: 16000 });
+
+            recorderRef.current.onstart = () => {
+              console.log('onstart: Recording started. Setting chunks to emmpty.')
+              setChunks([]);
+            };
+            recorderRef.current.ondataavailable = (e) => {
+              if (e.data.size > 0) {
+                setChunks((prev) => [...prev, e.data]);
+              } else {
+                // Empty chunk received, so we request new data after a short timeout
+                setTimeout(() => {
+                  if (recorderRef.current) recorderRef.current.requestData();
+                }, 25);
+              }
+            };
+
+            recorderRef.current.onstop = () => {
+              console.log('onstop: Recording stopped');
+              setIsRunning(false);
+              setIsProcessing(false);
+              seconds.reset();
+            };
+
+            // Start recording
+            recorderRef.current?.start();
+          })
+          .catch((err) => console.error('The following error occurred: ', err));
+      } else {
+        console.error('getUserMedia not supported on your browser!');
+      }
+      setShouldStartRecording(false);
+    }
+
+    return () => {
+      recorderRef.current?.stop();
+      recorderRef.current = null;
+      setShouldStartRecording(false);
+    };
+  }, [shouldStartRecording]);
+
+  // Process audio chunks
+  useEffect(() => {
+    if (!recorderRef.current) return;
+    if (!isRunning) return;
+    // if (status !== 'ready') return;
+    
+    if (chunks.length > 0) {
+      // Generate from data
+      console.log(
+        `Generating from ${chunks.length} chunks with mimetype ${recorderRef.current.mimeType}`
+      );
+      const blob = new Blob(chunks, { type: recorderRef.current.mimeType });
+
+      const fileReader = new FileReader();
+
+      fileReader.onloadend = async () => {
+        const arrayBuffer = fileReader.result;
+        if (!arrayBuffer || typeof arrayBuffer === 'string') return;
+        const decoded =
+          await audioContextRef.current?.decodeAudioData(arrayBuffer);
+        let audio = decoded?.getChannelData(0);
+        if (audio && audio.length > MAX_SAMPLES) {
+          // Get last MAX_SAMPLES
+          audio = audio.slice(-MAX_SAMPLES);
+        }
+
+        workerRef.current?.postMessage({
+          type: 'generate',
+          data: { audio, language: 'en' },
+        });
+      };
+      fileReader.readAsArrayBuffer(blob);
+    } else {
+      if (recorderRef.current?.state === 'recording') {
+        recorderRef.current.requestData();
+      }
+    }
+  }, [isRunning, isProcessing, chunks]);
+
+  const startRecording = async () => {
+    setShouldStartRecording(true);
+    setIsRunning(true);
+    setError(null);
+    // const stream = await navigator.mediaDevices.getUserMedia({
+    //   audio: true,
+    // });
+    // const recorder = new MediaRecorder(stream);
+    // let audioChunks: any = [];
+
+    // recorder.ondataavailable = (e) => {
+    //   audioChunks.push(e.data);
+    // };
+
+    // recorder.onstop = async () => {
+    //   const blob = new Blob(audioChunks, { type: 'audio/mp3' });
+    //   try {
+    //     const note = await createNote({
+    //       userId,
+    //       audioBlob: blob,
+    //     });
+
+    //     if (note) {
+    //       return push(`/notes/${note.id}`);
+    //     }
+    //   } catch (e) {
+    //     if (e instanceof Error) {
+    //       setError(e.message);
+    //     } else {
+    //       setError(
+    //         'There was an error with the transcription process, please try again.'
+    //       );
+    //     }
+    //     setIsRunning(false);
+    //     setIsProcessing(false);
+    //     seconds.reset();
+    //   }
+    //   recorderRef.current = null;
+    // };
+    // recorderRef.current = recorder;
+    // recorder.start(1000);
   };
 
   function stopRecording() {
-    if (!mediaRecorder) {
+    if (!recorderRef.current) {
       setIsRunning(false);
       return;
     }
 
-    mediaRecorder.stop();
+    recorderRef.current.stop();
     setIsRunning(false);
     setIsProcessing(true);
 
     // Stop all media tracks
-    mediaRecorder.stream.getTracks().forEach((track) => track.stop());
+    recorderRef.current.stream.getTracks().forEach((track) => track.stop());
   }
 
-  useInterval(seconds.increment, isRunning && mediaRecorder ? 1000 : null);
+  useInterval(
+    seconds.increment,
+    isRunning && recorderRef.current ? 1000 : null
+  );
 
   const handleRecordClick = () => {
     if (!isRunning) {
@@ -108,14 +258,21 @@ const Recorder = ({ userId }: { userId: string }) => {
     stopRecording();
   };
 
+  const displayTranscript = () => transcription && (
+    <div className="text-base font-normal text-center mx-auto mt-4 overflow-auto max-h-24" style={{lineHeight: '1.5em', maxHeight: '12em'}}>
+      {transcription}
+    </div>
+  )
+
   if (!isRunning && !isProcessing) {
     return (
       <div>
         <RecordButton
           onClick={handleRecordClick}
-          disabled={isRunning && !mediaRecorder}
+          disabled={isRunning && !recorderRef.current}
           isRunning={isRunning}
         />
+        {displayTranscript()}
         {error && (
           <p className="text-base font-normal text-center mx-auto mt-4 text-red-500">
             {error}
@@ -144,17 +301,21 @@ const Recorder = ({ userId }: { userId: string }) => {
         {(seconds.count % 60).toString().padStart(2, '0')}
       </div>
       <p className="text-base font-normal text-center mx-auto mt-4">{title}</p>
-      {((isRunning && !mediaRecorder) || isProcessing) && (
+      {((isRunning && !recorderRef.current) || isProcessing) && (
         <Image className="m-auto w-10" src={spinner} alt={'Loading'} />
       )}
-      <Waveform isProcessing={isProcessing} mediaRecorder={mediaRecorder} />
+      <Waveform
+        isProcessing={isProcessing}
+        mediaRecorder={recorderRef.current}
+      />
       {!isProcessing && (
         <RecordButton
           onClick={handleRecordClick}
-          disabled={isRunning && !mediaRecorder}
+          disabled={isRunning && !recorderRef.current}
           isRunning={isRunning}
         />
       )}
+      {displayTranscript()}
     </div>
   );
 };
